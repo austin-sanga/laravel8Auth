1c869a9392256b2a60a0afc76acd23f8bc088b6b
austin-sanga <austinsanga2000@gmail.com>
2022-04-15 19:34:26 +0300

    pasted auth

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+// this is essential for user Authenticating
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /* 
+    this is copied from the documentation
+
+     */
+    
+    /**
+     * Handle an authentication attempt.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+ 
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+ 
+            return redirect()->intended('dashboard');
+        }
+ 
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
+    }
+}
---
+++ resources/views/login.blade.php
@@ -0,0 +1,8 @@
+<h1>Login Page</h1>
+<form action="/login" method="POST">
+    <input type="text" name="email" placeholder="Email" required>
+    <br><br>
+    <input type="text" name="password" placeholder="Password" required>
+    <br><br>
+    <button type="submit">Submit</button>
+</form>
